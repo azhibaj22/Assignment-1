d91f78f7e92f0e6ef104136f839cf7bb7645738f
Aurora Zhibaj <azhibaj22@epoka.edu.al>
2023-04-02 20:22:50 +0200

    Added all codeblock files intact

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double areaCircle(int radius)
+{
+    return (double) radius*radius*3.141;
+}
+
+double perimeterCircle(int radius)
+{
+    return (double) 2*radius*3.141;
+}
+
+double areaOrPeri(char o)
+{
+    int r;
+    scanf("%d", &r);
+    if (o=='a')
+        return areaCircle(r);
+    else if (o=='p')
+        return perimeterCircle(r);
+    return 0;
+}
+
+int main()
+{
+    char aOp;
+    scanf("%c", &aOp);
+    double result = areaOrPeri(aOp);
+    printf("%.2lf", result);
+    return 0;
+}
